a0471451f54e44038d206e88f0e579c957b7b757
unknown <cheahwen1997@hotmail.com>
2021-06-24 11:53:09 +0800

    Implement Vertex Class

---
+++ WebGL/src/js/Vertex.js
@@ -0,0 +1,8 @@
+export class Vertex {
+    constructor(position, color, texcoord, normal){
+        this.position=position;
+        this.color=color;
+        this.texcoord=texcoord;
+        this.normal=normal;
+    }
+}
