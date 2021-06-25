bcf192d3d37a1a36b40b96baaa452ab78e78388c
looiyaowei <looiyaowei@gmail.com>
2021-06-25 19:14:10 +0800

    implemented Shader class

--- WebGL/src/index.js
+++ WebGL/src/index.js
@@ -6,6 +6,7 @@ import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
+import { Shader } from './js/Shader'
 
 // Appending to the DOM
 const logo = document.createElement('img')
@@ -18,12 +19,14 @@ const app = document.querySelector('#root')
 
 app.append(logo, heading)
 function main() {
-  //   var canvas = document.querySelector('#c')
-  //   var gl = canvas.getContext('webgl')
+  const canvas = document.querySelector('#canvas')
+  const gl = canvas.getContext('webgl')
   //   if (!gl) {
   //     return
   //   }
   //   const size = 2
   //   console.log(size)
+  const testing = new Shader(1, 1, gl)
+  testing.set1f('asd', 1.0)
 }
 main()
