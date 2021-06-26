0af6c95af899587d81a7a942d5f0d38de7ea2a0b
im28 <himaboy.im@gmail.com>
2021-06-26 17:32:40 +0800

    Updated Texture class

--- WebGL/src/js/Texture.js
+++ WebGL/src/js/Texture.js
@@ -1,18 +1,14 @@
-/** @type {HTMLCanvasElement}  For Autocomplete*/
-var canvas = document.querySelector("#canvas");
-var gl = canvas.getContext("webgl");
 export class Texture{
-    constructor(url,type){
+    constructor(url,type,gl){
         this.type = type;
-        
+        /** @type {WebGL2RenderingContext}  For Autocomplete*/
+        this.gl = gl
         this.texture = gl.createTexture();
-        //gl.bindTexture(gl.TEXTURE_2D, this.texture);
 
-        //maybe change to this.type
-        gl.texParameteri(type,gl.TEXTURE_WRAP_S,gl.REPEAT);
-        gl.texParameteri(type,gl.TEXTURE_WRAP_T,gl.REPEAT);
-        gl.texParameteri(type,gl.TEXTURE_MAG_FILTER,gl.LINEAR_MIPMAP_NEAREST);
-        gl.texParameteri(type,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
+        gl.texParameteri(this.type,gl.TEXTURE_WRAP_S,gl.REPEAT);
+        gl.texParameteri(this.type,gl.TEXTURE_WRAP_T,gl.REPEAT);
+        gl.texParameteri(this.type,gl.TEXTURE_MAG_FILTER,gl.LINEAR_MIPMAP_NEAREST);
+        gl.texParameteri(this.type,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
 
 
         // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
@@ -32,13 +28,13 @@ export class Texture{
     }
 
     bind(texture_unit){
-        gl.activeTexture(gl.TEXTURE0+texture_unit);
-        glBindTexture(this.type, this.texture);
+        this.gl.activeTexture(this.gl.TEXTURE0+texture_unit);
+        this.gl.bindTexture(this.type, this.texture);
     }
 
     unbind(){
-        gl.activeTexture(0);
-        glBindTexture(this.type, 0);
+        this.gl.activeTexture(null);
+        this.gl.bindTexture(this.type, null);
     }
 
 }
